7b24e3fc1bd2dd4cc3f9a7085c032ef9eb712ed7
zanderp25 <zanderpicon@outlook.com>
2020-12-11 08:35:12 -0500

    first commit

---
+++ html/index.js
@@ -0,0 +1,28 @@
+function load(){
+  location.protocol = "https:";
+}
+function share(){
+  navigator.share({
+    title: 'Zanderp25',
+    text: 'Check out Zanderp25\'s website!',
+    url: 'http://zanderp25.com/',
+  })
+    .then(() => console.log('Successful share'))
+    .catch((error) => console.log('Error sharing', error));
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// Turn it down for what?
+function turnDown() {
+  document.getElementById('tdf').innerText="Turn it down... For what?";
+  document.getElementsByClassName('header').innerText="Turn it down... For what?";
+  var s = document.createElement('script');
+  s.setAttribute('src','https://nthitz.github.io/turndownforwhatjs/tdfw.js');
+  document.body.appendChild(s);
+}
