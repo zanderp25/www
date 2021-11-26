824b120a6bd90ff0f6d16b9fe3ee3337db3b06fb
zanderp25 <zanderp@staff.clicksminuteper.net>
2021-11-26 13:04:59 -0500

    Update pull.php

--- html/utils/pull.php
+++ html/utils/pull.php
@@ -3,7 +3,7 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-shell_exec( 'cd /home/zanderp25/www/ && git pull' );
+shell_exec( 'cd /home/zanderp25/www/ && git reset –hard HEAD && git pull' );
 }
 
 ?>hi
