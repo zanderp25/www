64f73912ae71dab474b0a3429215762781656afc
zanderp25 <zanderp@staff.clicksminuteper.net>
2021-11-27 00:14:34 -0500

    ok this should work

--- html/utils/pull.php
+++ html/utils/pull.php
@@ -3,7 +3,10 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-shell_exec( ‘ cd /home/zanderp25/www/ && git reset –hard HEAD && git pull’ );
+  shell_exec("cd /home/zanderp25/www/ && git reset –hard HEAD && git pull");
+  echo "Done"
+} else {
+  echo "hi"
 }
 
-?>hi
+?>
