f8f7e1db2f666447e1af219b31f883acdb0d4441
zanderp25 <zanderpicon@outlook.com>
2021-11-28 15:26:03 -0500

    Update autopull to show errors and actually error when somethigs wrong

--- html/utils/pull.php
+++ html/utils/pull.php
@@ -1,10 +1,28 @@
 <?php
-  $output = null;
-  $value = null;
+function run(){
+    $output = null;
+    $value = null;
+  
+    exec("cd /home/zanderp25/www; git pull", $output, $value);
+    echo "Returned with code: $value";
+    echo "Output: $output";
+    if ($value===0){
+      echo "Success";
+      http_response_code(200);
+    } else {
+      echo "Failed";
+      http_response_code(500);
+    }
+}
 
-  exec("cd /home/zanderp25/www; git pull", $output, $value);
-  echo "Returned with code: <pre style=display:inline >$value</pre><br/>";
-  echo "Output:<pre>";
-  print_r($output);
-  echo "</pre>"
+try {
+    if (!isset($_POST['payload'])) {
+        echo "Hi";
+    } else {
+        run();
+    }
+} catch ( Exception $e ) {
+    echo "Error: $e->getMessage()";
+    http_response_code(500);
+}
 ?>
