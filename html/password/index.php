f723ae2bd28c8651b00ff4e4d5cb8c4d131a7d3b
zanderp25 <zanderpicon@outlook.com>
2021-11-28 22:20:48 -0500

    forgot semicolons

--- html/password/index.php
+++ html/password/index.php
@@ -3,13 +3,13 @@ if (isset($_GET['password'])) {
     if ($_GET['password']==="nevergonnagiveyouup") {
         echo "Correct";
         if ($_SERVER['REMOTE_ADDR'] === "24.179.149.1") {
-            echo "\nCongrats, Clari. You are the only one that can see this specific message."
+            echo "\nCongrats, Clari. You are the only one that can see this specific message.";
         }
         http_response_code(200);
     } else {
         echo "No";
         if ($_SERVER['REMOTE_ADDR'] === "24.179.149.1") {
-            echo "\nHi Clari"
+            echo "\nHi Clari";
         }
         http_response_code(403);
     }
