618e32f41b39f792f77ce0477be523d909f4ad11
zanderp25 <zanderpicon@outlook.com>
2021-11-28 20:46:41 -0500

    ok no secret ig

--- html/portal/index.php
+++ html/portal/index.php
@@ -1,11 +1,3 @@
 <?php
-function run(){
     echo "haha sorry you missed out if you want to see what it was, go here: <a href=\"#\" onclick=\"location='file.php'\">click me</a>"
-}
-
-try{
-    run()
-} catch ( Exception $e ) {
-    echo "Error: $e->getMessage()";
-}
 ?>
