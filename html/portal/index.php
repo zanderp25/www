c2e7666eb596996d313750ac9155549fe3ba9456
zanderp25 <zanderpicon@outlook.com>
2021-11-28 20:38:43 -0500

    hehe guess my password

--- html/portal/index.php
+++ html/portal/index.php
@@ -1,6 +1,12 @@
 <?php
-if ($_GET["e"] === "secret"){
-    echo "Congrats, you found a secret page."
+if (isset($_GET["e"])){
+    if ($_GET["e"] === "secret"){
+        echo "Congrats, you found a secret page. <a href=\"#\" onclick=\"location='file.php'\">click me</a>"
+        http_response_code(200);
+    } else {
+        echo "incorrect password"
+        http_response_code(403);
+    }
 } else {
     echo "haha sorry you missed out if you want to see what it was, go here: <a href=\"#\" onclick=\"location='file.php'\">click me</a>"
 }
