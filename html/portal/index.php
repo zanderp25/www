97d9afab824ec619d3037fb1a57d00601103e30a
zanderp25 <zanderpicon@outlook.com>
2021-11-28 20:42:08 -0500

    ok why is it not working smh

--- html/portal/index.php
+++ html/portal/index.php
@@ -1,13 +1,22 @@
 <?php
-if (isset($_GET["e"])){
-    if ($_GET["e"] === "secret"){
-        echo "Congrats, you found a secret page. <a href=\"#\" onclick=\"location='file.php'\">click me</a>"
-        http_response_code(200);
+function run(){
+    if (isset($_GET["e"])){
+        if ($_GET["e"] === "secret"){
+            echo "Congrats, you found a secret page. <a href=\"#\" onclick=\"location='file.php'\">click me</a>"
+            http_response_code(200);
+        } else {
+            echo "incorrect password"
+            http_response_code(403);
+        }
     } else {
-        echo "incorrect password"
-        http_response_code(403);
+        echo "haha sorry you missed out if you want to see what it was, go here: <a href=\"#\" onclick=\"location='file.php'\">click me</a>"
     }
-} else {
-    echo "haha sorry you missed out if you want to see what it was, go here: <a href=\"#\" onclick=\"location='file.php'\">click me</a>"
+}
+
+try{
+    run()
+} catch ( Exception $e ) {
+    echo "Error: $e->getMessage()";
+    http_response_code(500);
 }
 ?>
