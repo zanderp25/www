6edc5dd715f52e99e05c3d0b14905094f65bdb64
zanderp25 <zanderpicon@outlook.com>
2021-11-28 21:31:05 -0500

    ok *now* guess my password

---
+++ html/password/index.php
@@ -0,0 +1,13 @@
+<?php
+if (isset($_GET['password']){   
+    if($_GET['password']==="nevergonnagiveyouup"){
+        echo "Correct";
+        http_response_code(200);
+    } else {
+        echo "No";
+        http_response_code(403);
+    }
+} else {
+    echo "Use '?password=' to guess my password.";
+}
+?>
