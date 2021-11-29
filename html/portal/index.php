13a7c634edfea674bf600ddc45b426cae5112dc9
zanderp25 <zanderpicon@outlook.com>
2021-11-28 20:45:40 -0500

    ok what the heck

--- html/portal/index.php
+++ html/portal/index.php
@@ -1,16 +1,6 @@
 <?php
 function run(){
-    if (isset($_GET["e"])){
-        if ($_GET["e"] === "secret"){
-            echo "Congrats, you found a secret page. <a href=\"#\" onclick=\"location='file.php'\">click me</a>"
-            http_response_code(200);
-        } else {
-            echo "incorrect password"
-            http_response_code(403);
-        }
-    } else {
-        echo "haha sorry you missed out if you want to see what it was, go here: <a href=\"#\" onclick=\"location='file.php'\">click me</a>"
-    }
+    echo "haha sorry you missed out if you want to see what it was, go here: <a href=\"#\" onclick=\"location='file.php'\">click me</a>"
 }
 
 try{
