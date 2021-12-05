740da336ff40a377ae992b6493bd78e9101c553b
zanderp25 <zanderp@staff.clicksminuteper.net>
2021-12-05 12:21:06 -0500

    eliminate remnants of the share button

--- html/new/index.js
+++ html/new/index.js
@@ -1,20 +1,3 @@
 function load() {
   // i had an https redirect here but i dont need it anymore
 }
-
-function shareTest(){
-  if(navigator.share){
-    shareButton = document.getElementById("share");
-    shareButton.hidden = false;
-  }
-}
-
-function share(){
-  navigator.share({
-    title: 'Zanderp25',
-    text: 'Check out Zanderp25\'s website!',
-    url: 'https://zanderp25.com/',
-  })
-    .then(() => console.log('Successful share'))
-    .catch((error) => console.log('Error sharing', error));
-}
