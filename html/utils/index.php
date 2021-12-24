0cd201486257e4a4411174ce6895c2bfb0c28a15
zanderp25 <zanderp@staff.clicksminuteper.net>
2021-12-23 21:54:29 -0500

    added an index to utils

---
+++ html/utils/index.php
@@ -0,0 +1,8 @@
+<?php
+if ($_SERVER['HTTP_USER_AGENT'] == "Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)") {
+    header("Location: https://zanderp25.com/new/");
+} else {
+    header("Location: https://youtu.be/dQw4w9WgXcQ");
+}
+exit();
+?>
