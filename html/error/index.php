2d5c2729ff55002cdb48e625363061eb79b4458f
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-01-03 14:52:00 -0500

    deleted elseif stuf

--- html/error/index.php
+++ html/error/index.php
@@ -4,52 +4,9 @@
   } else {
     $e = $_GET['e'];
   }
-  $string = $e." - Unknown Error";
-  $info = "The server encountered an unexpected condition which prevented it from fulfilling your request.";
+  $string = "Unknown Error";
+  $info = "The server encountered an unexpected condition which prevented it from fulfilling your request. Error code: ".$e;
   
-  // if ($e === "401") {
-  //   $string = "401 - Unauthorized";
-  //   $info = "You are not authorized to access this page.";
-  // } else if ($e === "403") {
-  //   $string = "403 - Forbidden";
-  //   $info = "You don't have permission to access this page.";
-  // } else if ($e === "404") {
-  //   $string = "404 - Not Found";
-  //   $info = "The requested URL was not found on this server.";
-  // } else if ($e === "410") {
-  //   $string = "410 - Gone";
-  //   $info = "The requested URL is no longer available on this server.";
-  // } else if ($e === "414") {
-  //   $string = "414 - Request-URI Too Long";
-  //   $info = "The request URI is too long.";
-  // } else if ($e === "420") {
-  //   $string = "420 - Enhance Your Calm";
-  //   $info = "The request was well-formed but was unable to be followed due to semantic errors.";
-  // } else if ($e === "497") {
-  //   $string = "497 - HTTP Request Sent to HTTPS Port";
-  //   $info = "The request was sent to a port on a server that requires the use of SSL.";
-  // } else if ($e === "500") {
-  //   $string = "500 - Internal Server Error";
-  //   $info = "The server encountered an unexpected condition which prevented it from fulfilling your request.";
-  // } else if ($e === "501") {
-  //   $string = "501 - Not Implemented";
-  //   $info = "The server does not support the functionality required to fulfill your request.";
-  // } else if ($e === "502") {
-  //   $string = "502 - Bad Gateway";
-  //   $info = "The server, while acting as a gateway or proxy, received an invalid response from the upstream server it accessed in attempting to fulfill your request.";
-  // } else if ($e === "503") {
-  //   $string = "503 - Service Unavailable";
-  //   $info = "The server is currently unable to handle the request due to a temporary overloading or maintenance of the server.";
-  // } else if ($e === "504") {
-  //   $string = "504 - Gateway Timeout";
-  //   $info = "The server, while acting as a gateway or proxy, did not receive a timely response from the upstream server specified by the URI (e.g. HTTP, FTP, LDAP) or some other auxiliary server (e.g. DNS) it needed to access in attempting to complete the request.";
-  // } else if ($e === "507") {
-  //   $string = "507 - Insufficient Storage";
-  //   $info = "The server does not have enough space to store the resource.";
-  // } else if ($e === "509") {
-  //   $string = "509 - Bandwidth Limit Exceeded";
-  //   $info = "The server terminated an operation because it encountered an infinite loop while processing.";
-  // }
   switch ($e) {
     case "401":
       $string = "401 - Unauthorized";
