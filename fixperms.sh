e7d95a28565ea63e936a47820365280463f85953
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-01-28 23:20:25 -0500

    php specific permissions sound nice

--- fixperms.sh
+++ fixperms.sh
@@ -1,3 +1,4 @@
 sudo chown -R zanderp25:www-data .
 sudo find . -type d -print0 | xargs -0 chmod 755
 sudo find . -type f -print0 | xargs -0 chmod 644
+sudo find . -type f -name "*.php" | xargs -0 chmod 0640
