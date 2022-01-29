4270881adffcffe57d197e9abbee9f788a31cd3c
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-01-28 23:24:17 -0500

    i would like to run my script tyvm

--- fixperms.sh
+++ fixperms.sh
@@ -1,4 +1,4 @@
 sudo chown -R zanderp25:www-data .
-sudo find . -type d -print0 | xargs -0 chmod 755
-sudo find . -type f -print0 | xargs -0 chmod 644
-sudo find . -type f -name "*.php" | xargs -0 chmod 0640
+sudo find html link -type d -print0 | xargs -0 chmod 755
+sudo find html link -type f -print0 | xargs -0 chmod 644
+sudo find html link -type f -name "*.php" -print0 | xargs -0 chmod 0640
