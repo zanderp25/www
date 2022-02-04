ac0b787c754096e36ecc38b05a395770de07327d
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-02-03 19:24:41 -0500

    fix the link thing so i can rickroll more people

--- html/utils/index.php
+++ html/utils/index.php
@@ -1,6 +1,7 @@
 <?php
-if ($_SERVER['HTTP_USER_AGENT'] == "Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)") {
-    header("Location: https://zanderp25.com/new/");
+$agent = $_SERVER['HTTP_USER_AGENT'];
+if (strpos($agent, "Discordbot") !== false || strpos($agent, "Twitterbot") !== false || strpos($agent, "facebookexternalhit") !== false) {
+    header("Location: https://zanderp25.com/");
 } else {
     header("Location: https://youtu.be/dQw4w9WgXcQ");
 }
