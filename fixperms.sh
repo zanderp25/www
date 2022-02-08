86946a6eccb36c56298a3424980f463c18ce2848
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-02-08 14:47:56 -0500

    fix folder permissisons

--- fixperms.sh
+++ fixperms.sh
@@ -1,5 +1,5 @@
 sudo chown -R zanderp25:www-data .
-sudo find html link -type d -print0 | xargs -0 chmod 755
+sudo find html link -type d -print0 | xargs -0 chmod 775
 sudo find html link -type f -print0 | xargs -0 chmod 644
 sudo find html link -type f -name "*.php" -print0 | xargs -0 chmod 0640
 sudo chmod -R 770 .git
