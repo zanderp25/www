9f10e566aac6ad321910a8ba8d4b025974ec6b27
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-02-13 22:57:09 -0500

    the indentation bothered me lol

--- html/utils/pull.php
+++ html/utils/pull.php
@@ -2,17 +2,17 @@
 function run(){
     $output = null;
     $value = null;
-  
+    
     exec("cd /home/zanderp25/www; git pull 2>&1", $output, $value);
     echo "Returned with code: $value\n";
     echo "Output: \n";
     print_r($output);
     if ($value === 0){
-      echo "Success";
-      http_response_code(200);
+        echo "Success";
+        http_response_code(200);
     } else {
-      echo "Failed";
-      http_response_code(500);
+        echo "Failed";
+        http_response_code(500);
     }
 }
 
