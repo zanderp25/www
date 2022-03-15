05509bea2f2eee47d40224cac04bfae6d288eae3
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-03-15 17:14:15 -0400

    made a description so it would show an embed

--- html/color/index.php
+++ html/color/index.php
@@ -24,6 +24,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content=<?php echo $color; ?>>
+    <meta name="description" content="A simplistic color picker i made">
     <title><?php 
         echo "$color - Color Picker";
     ?></title>
