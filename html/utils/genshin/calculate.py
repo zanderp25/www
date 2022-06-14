f696be5d0508143110dcda1a9b9ef1b973c2b67b
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-06-14 00:46:02 -0400

    fixed genshin reset api

--- html/utils/genshin/calculate.py
+++ html/utils/genshin/calculate.py
@@ -1,8 +1,15 @@
 from datetime import datetime, timedelta, time
-import json
+import json, sys, pytz
+
+debug = "-d" in sys.argv
+
+current_time = datetime.now(pytz.utc)
+if debug and len(sys.argv)==4:
+    strdate = ' '.join(sys.argv[2:4])
+    current_time = datetime.strptime(strdate, "%Y-%m-%d %H:%M:%S")
+    current_time = current_time.replace(tzinfo=pytz.utc)
 
 def daily() -> datetime:
-    current_time = datetime.utcnow()
     hours = 9 if current_time.hour < 9 else 33
     midnight = current_time.replace(
         hour=0,
@@ -12,23 +19,25 @@ def daily() -> datetime:
     )
     return midnight + timedelta(hours=hours)
 
-#print(daily())
+if debug: print(daily())
 
 def weekly() -> datetime:
-    current_time = datetime.utcnow()
     midnight = current_time.replace(
         hour=0,
         minute=0,
         second=0,
         microsecond=0,
     )
-    return midnight + timedelta(
-        hours=9,
-        days=-current_time.weekday(),
-        weeks=1,
-    )
+    if current_time.hour < 9 and current_time.weekday() == 0:
+        return midnight + timedelta(hours=9)
+    else:
+        return midnight + timedelta(
+            hours=9,
+            days=-current_time.weekday(),
+            weeks=1,
+        )
 
-#print(weekly())
+if debug: print(weekly())
 
 data = {
     'daily': round(datetime.timestamp(daily())),
