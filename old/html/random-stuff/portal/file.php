eebadcddb9630b8351e9ebe2e92443fa74a9060c
zanderp25 <zander@zanderp25.com>
2022-10-08 00:21:23 -0400

    remove old pages

--- old/html/random-stuff/portal/file.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Refresh: 10 https://youtu.be/dQw4w9WgXcQ');
-
-?>
-<title>Zanderp25</title>
-<meta name="description" content="A cool creation.">
-<meta name="author" content="Zanderp25">
-Hello Yello!
