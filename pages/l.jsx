1a01dcb3d52d23cc21cf3d27bde57d4b0879fe68
zanderp25 <zander@zanderp25.com>
2022-11-27 23:11:53 -0500

    hehe funny

--- pages/l.jsx
+++ pages/l.jsx
@@ -1,9 +1,29 @@
-// import { Redirect } from "next";
+export async function getServerSideProps({ req }) {
+  let isBot = (req
+    ? req.headers['user-agent']
+    : navigator.userAgent).match(
+      /Discordbot|Twitterbot|facebookexternalhit/i
+    )
+
+  if (isBot) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  } else {
+    return {
+      redirect: {
+        destination: 'https://youtu.be/dQw4w9WgXcQ',
+        permanent: false,
+      },
+    }
+  }
+}
 
 export default function RickRoll(){
-  return (
-    <>
-      {/* Insert redirect stuff here or smth */}
-    </>
-  );
+  return(
+    <p>hi</p>
+  )
 }
