6af2eaac8e2e11a58a5ecd43630b032e4646e523
zanderp25 <zander@zanderp25.com>
2022-12-03 02:43:00 -0500

    add cwd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
     apps : [{
         name   : "Website",
         script : "./node_modules/next/dist/bin/next",
-        args   : "start -p 2999"
+        args   : "start -p 2999",
+        cwd   : "/home/zanderp25/www",
     }]
 }
