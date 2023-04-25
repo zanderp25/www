634bf2ee7a57e475bf751888d043d96362aaf7d7
zanderp25 <zander@zanderp25.com>
2023-04-25 11:59:01 -0400

    let pm2 build at start time so i can do a pm2 pull

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     apps : [{
         name   : "Website",
-        script : "./node_modules/next/dist/bin/next",
-        args   : "start -p 2999",
+        script : "/usr/local/bin/yarn",
+        args   : "start",
         cwd   : "/home/zanderp25/www",
     }]
 }
