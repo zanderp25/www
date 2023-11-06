09ae15e2bc3a800c08dc511fae3ba5a7cb66d457
zanderp25 <zander@zanderp25.com>
2023-11-05 21:09:25 -0500

    dammit case sensitive linux

--- components/countdown.tsx
+++ components/countdown.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "../styles/Components/countdown.module.css";
-import popoverStyles from '../styles/components/countdown.ver.module.css';
+import popoverStyles from '../styles/Components/countdown.ver.module.css';
 
 export function Countdown({ date, light }: { date: Date|string, light?: boolean }): JSX.Element {
     light = !!light;
