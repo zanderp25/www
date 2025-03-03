bacc0f9357b52ca6b25653c0fe25df6f68972360
Zander Picon <zander@zanderp25.com>
2025-03-03 13:12:54 -0500

    fix genshin api

--- pages/api/genshin/index.tsx
+++ pages/api/genshin/index.tsx
@@ -1,13 +1,18 @@
 export default async function handler(req, res) {
     let now = new Date();
-    let daily = new Date(now.getTime() + 9 * 60 * 60 * 1000);
+    
+    // Calculate the next day at 4am UTC-5 (9am UTC)
+    let daily = new Date(now.getTime() + 24 * 60 * 60 * 1000);
     daily.setUTCHours(9);
     daily.setUTCMinutes(0);
     daily.setUTCSeconds(0);
     daily.setUTCMilliseconds(0);
 
+    // Calculate the next Monday at 9am UTC
     let weekly = new Date(daily.getTime());
-    weekly.setUTCDate(daily.getUTCDate() + (7 - daily.getUTCDay() + 1) % 7);
+    let day = weekly.getUTCDay();
+    let diff = (day <= 1) ? 1 - day : 8 - day;
+    weekly.setUTCDate(weekly.getUTCDate() + diff);
 
-    res.status(200).json({ daily: daily.getTime()/1000, weekly: weekly.getTime()/1000 });
+    res.status(200).json({ daily: daily.getTime() / 1000, weekly: weekly.getTime() / 1000 });
 }
