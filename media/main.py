b510533e67574dad0b4388af9fbb35e712adb84c
Zander Picon <zander@zanderp25.com>
2025-11-08 14:05:31 -0500

    fix double slash bug

--- media/main.py
+++ media/main.py
@@ -32,7 +32,7 @@ def media(path):
             file_size = os.path.getsize(os.path.join('media', file_path))
             is_dir = os.path.isdir(os.path.join('media', file_path))
             file_path = file_path.replace(' ', '%20') # for URLs
-            file_data += f'{{"name":"{file}", "size": {file_size}, "path": "/{file_path}", "isDir": {"true" if is_dir else "false"}}},'
+            file_data += f'{{"name":"{file}", "size": {file_size}, "path": "{file_path}", "isDir": {"true" if is_dir else "false"}}},'
         file_data = file_data.rstrip(',')
         pattern = r"\/\/ DATA START\n([\s\S]*)\n\/\/ DATA END"
         data = f'// DATA START\nlet pwd = "/{path}";\nlet files = [{file_data}];\n// DATA END'
