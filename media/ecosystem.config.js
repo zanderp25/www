cfed69d5dd88366be777e0ab54234e5c1978d22e
zanderp25 <zander@zanderp25.com>
2026-02-08 21:19:27 -0500

    Move Media to its own repo

--- media/ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apps : [{
-        name       : "Media",
-        script     : "./main.py",
-        interpreter: "python3",
-        cwd        : "/home/zanderp25/www/media",
-    }]
-}
